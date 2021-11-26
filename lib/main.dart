31f693342880aa022c2d9f45ff3c9665f7be69f9
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-26 17:28:32 +0000

    primary swatch

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,29 @@ class MediaQ extends StatelessWidget {
 }
 
 class MyApp extends StatelessWidget {
+   int _blackPrimaryValue = 0xFF000000;
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return MaterialApp(
       title: 'Accelerometer',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(
+        _blackPrimaryValue,
+        <int, Color>{
+          50: Color(0xFF000000),
+          100: Color(0xFF000000),
+          200: Color(0xFF000000),
+          300: Color(0xFF000000),
+          400: Color(0xFF000000),
+          500: Color(_blackPrimaryValue),
+          600: Color(0xFF000000),
+          700: Color(0xFF000000),
+          800: Color(0xFF000000),
+          900: Color(0xFF000000),
+        },
+      )
+      
       ),
       home: TestParallax(title: 'Accelerometer'),
     );
