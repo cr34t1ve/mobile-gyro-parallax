0dc58ed5b72f5bcc9360127f5791b0fa9790dea0
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-26 12:39:03 +0000

    working test

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,26 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:mobile_gyro_parallax/anothertest.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Accelerometer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnotherTest(title: 'Accelerometer'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,68 +29,210 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // color of the circle
+  Color color = Colors.greenAccent;
 
-  void _incrementCounter() {
+  // event returned from accelerometer stream
+  AccelerometerEvent? event;
+
+  // hold a refernce to these, so that they can be disposed
+  Timer? timer;
+  StreamSubscription? accel;
+
+  // positions and count
+  double top = 125;
+  double? left;
+  int count = 0;
+
+  // variables for screen size
+  double? width;
+  double? height;
+
+  setColor(AccelerometerEvent event) {
+    // Calculate Left
+    double x = ((event.x * 12) + ((width! - 100) / 2));
+    // Calculate Top
+    double y = event.y * 12 + 125;
+
+    // find the difference from the target position
+    var xDiff = x.abs() - ((width! - 100) / 2);
+    var yDiff = y.abs() - 125;
+
+    // check if the circle is centered, currently allowing a buffer of 3 to make centering easier
+    if (xDiff.abs() < 3 && yDiff.abs() < 3) {
+      // set the color and increment count
+      setState(() {
+        color = Colors.greenAccent;
+        count += 1;
+      });
+    } else {
+      // set the color and restart count
+      setState(() {
+        color = Colors.red;
+        count = 0;
+      });
+    }
+  }
+
+  setPosition(AccelerometerEvent event) {
+    if (event == null) {
+      return;
+    }
+
+    // When x = 0 it should be centered horizontally
+    // The left positin should equal (width - 100) / 2
+    // The greatest absolute value of x is 10, multipling it by 12 allows the left position to move a total of 120 in either direction.
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      left = ((event.x * 12) + ((width! - 100) / 2));
+    });
+
+    // When y = 0 it should have a top position matching the target, which we set at 125
+    setState(() {
+      top = event.y * 12 + 125;
+    });
+  }
+
+  startTimer() {
+    // if the accelerometer subscription hasn't been created, go ahead and create it
+    if (accel == null) {
+      accel = accelerometerEvents.listen((AccelerometerEvent eve) {
+        setState(() {
+          event = eve;
+        });
+      });
+    } else {
+      // it has already ben created so just resume it
+      accel!.resume();
+    }
+
+    // Accelerometer events come faster than we need them so a timer is used to only proccess them every 200 milliseconds
+    if (timer == null || !timer!.isActive) {
+      timer = Timer.periodic(Duration(milliseconds: 5), (_) {
+        // if count has increased greater than 3 call pause timer to handle success
+        if (count > 3) {
+          pauseTimer();
+        } else {
+          // proccess the current event
+          setColor(event!);
+          setPosition(event!);
+        }
+      });
+    }
+  }
+
+  pauseTimer() {
+    // stop the timer and pause the accelerometer stream
+    timer!.cancel();
+    accel!.pause();
+
+    // set the success color and reset the count
+    setState(() {
+      count = 0;
+      color = Colors.green;
     });
   }
 
   @override
+  void dispose() {
+    timer?.cancel();
+    accel?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // get the width and height of the screen
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Keep the circle in the center for 1 second'),
+          ),
+          Stack(
+            children: [
+              // This empty container is given a width and height to set the size of the stack
+              Container(
+                height: height! / 2,
+                width: width,
+              ),
+
+              // Create the outer target circle wrapped in a Position
+              Positioned(
+                // positioned 50 from the top of the stack
+                // and centered horizontally, left = (ScreenWidth - Container width) / 2
+                top: 50,
+                left: (width! - 250) / 2,
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red, width: 2.0),
+                    borderRadius: BorderRadius.circular(125),
+                  ),
+                ),
+              ),
+              // This is the colored circle that will be moved by the accelerometer
+              // the top and left are variables that will be set
+              Positioned(
+                top: top,
+                left: left ?? (width! - 100) / 2,
+                // the container has a color and is wrappeed in a ClipOval to make it round
+                child: ClipOval(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: color,
+                  ),
+                ),
+              ),
+              // second colored circle for testing parallax
+              Positioned(
+                top: top *= -1,
+                left: left ?? (width! - 100) / 2,
+                // the container has a color and is wrappeed in a ClipOval to make it round
+                child: ClipOval(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              // inner target circle wrapped in a Position
+              Positioned(
+                top: 125,
+                left: (width! - 100) / 2,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green, width: 2.0),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text('x: ${(event?.x ?? 0).toStringAsFixed(3)}'),
+          Text('y: ${(event?.y ?? 0).toStringAsFixed(3)}'),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: RaisedButton(
+              onPressed: startTimer,
+              child: Text('Begin'),
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
